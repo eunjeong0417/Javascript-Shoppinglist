67cdefa70edb6a486217bfdf05ab32014a50f190
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-09 23:02:39 +0900

    2 commit

--- index.js
+++ index.js
@@ -2,39 +2,29 @@ const inputEl = document.querySelector('.input-el')
 const plusBtn = document.querySelector('.btn_plus')
 const ulEl = document.querySelector('.ul-el')
 
-inputEl.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        const li = document.createElement("li")
-    li.setAttribute('class', 'li-el')
-        li.innerText = inputEl.value;
-        
-    const checkBtn = document.createElement("button")
-    checkBtn.setAttribute('class', 'check-btn')
-    checkBtn.innerHTML = '<i class="fas fa-check"></i>';
-    checkBtn.addEventListener('click', () => {
-        li.classList.add('line-through')
-    })
 
-    const trashBtn = document.createElement("button")
-    trashBtn.innerHTML = '<i class="fas fa-trash"></i>'
-    trashBtn.addEventListener('click', () => {
-        trashBtn.parentElement.remove()
-    })    
-
-    li.appendChild(checkBtn)
-    li.appendChild(trashBtn)
-    ulEl.appendChild(li)
-
-
-    inputEl.value = ""
-    inputEl.focus()
+inputEl.addEventListener("keypress", (e) => {
+    if (e.keyCode === 13 && inputEl.value) {
+        console.log('클릭')
+    } else {
+        alert('입력')
     }
 })
 
+
 plusBtn.addEventListener('click', () => {
+
+//input value가 있으면 리스트에 추가하고
+//없으면 alert창 띄운다
+    if (inputEl.value) {
+
     const li = document.createElement("li")
+//li 태그를 만든다
     li.setAttribute('class', 'li-el')
+//만든 li태그에 li-el의 class 속성을 준다
     li.innerText = inputEl.value
+//li태그 안에 입력받은 input의 value 값을 넣어준다
+
 
     const checkBtn = document.createElement("button")
     checkBtn.setAttribute('class', 'check-btn')
@@ -43,17 +33,32 @@ plusBtn.addEventListener('click', () => {
         li.classList.add('line-through')
     })
 
+
     const trashBtn = document.createElement("button")
     trashBtn.innerHTML = '<i class="fas fa-trash"></i>'
+    trashBtn.setAttribute('class', 'trash-btn')
     trashBtn.addEventListener('click', () => {
         trashBtn.parentElement.remove()
     })
+    
 
+//만들어둔 버튼을 list 태그에 넣어준다
     li.appendChild(checkBtn)
     li.appendChild(trashBtn)
+//버튼이 추가된 list 태그를 ul 태그에 넣어준다
     ulEl.appendChild(li)
 
 
     inputEl.value = ""
+//입력을 한 뒤에는 input창을 비운다
     inputEl.focus()
+//input에 focus를 시켜준다
+        
+    }
+    else {
+        alert('쇼핑목록을 입력하세요')
+        inputEl.focus()
+    }
+
 })
+    
