aa45d52e0f3470872ddd761f6ed0afae7073d2d3
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-10 00:31:05 +0900

    list태그 수정

--- index.js
+++ index.js
@@ -3,16 +3,25 @@ const plusBtn = document.querySelector('.btn_plus')
 const ulEl = document.querySelector('.ul-el')
 
 
+plusBtn.addEventListener('click', onAdd)
+
 inputEl.addEventListener("keypress", (e) => {
+//keypress 이벤트
+//13은 엔터키의 코드를 의미한다
+//엔터키가 눌려지고 input의 값이 있다면 onAdd 함수를 실행
     if (e.keyCode === 13 && inputEl.value) {
-        console.log('클릭')
+        onAdd()       
     } else {
-        alert('입력')
+        alert('쇼핑목록을 입력하세요')
+        inputEl.focus()
+//input에 focus를 넣어준다
     }
 })
 
 
-plusBtn.addEventListener('click', () => {
+
+
+function onAdd () {
 
 //input value가 있으면 리스트에 추가하고
 //없으면 alert창 띄운다
@@ -24,7 +33,6 @@ plusBtn.addEventListener('click', () => {
 //만든 li태그에 li-el의 class 속성을 준다
     li.innerText = inputEl.value
 //li태그 안에 입력받은 input의 value 값을 넣어준다
-
 
     const checkBtn = document.createElement("button")
     checkBtn.setAttribute('class', 'check-btn')
@@ -41,11 +49,23 @@ plusBtn.addEventListener('click', () => {
         trashBtn.parentElement.remove()
     })
     
+      
+    const arrowBtn = document.createElement("button")
+    arrowBtn.innerHTML = '<i class="fas fa-arrow-up"></i>'
+    arrowBtn.setAttribute('class', 'arrowBtn')
+    
+        const divEl = document.createElement("div")
+        divEl.setAttribute('class', 'divEl')
 
-//만들어둔 버튼을 list 태그에 넣어준다
-    li.appendChild(checkBtn)
-    li.appendChild(trashBtn)
-//버튼이 추가된 list 태그를 ul 태그에 넣어준다
+//div태그에 각각의 버튼을 넣어준다
+    divEl.appendChild(checkBtn)
+    divEl.appendChild(trashBtn)
+    divEl.appendChild(arrowBtn)
+        
+//div태그를 list태그에 넣는다
+    li.appendChild(divEl)
+  
+//마지막으로 ul태그에 list태그를 넣어준다
     ulEl.appendChild(li)
 
 
@@ -60,5 +80,5 @@ plusBtn.addEventListener('click', () => {
         inputEl.focus()
     }
 
-})
+}
     
